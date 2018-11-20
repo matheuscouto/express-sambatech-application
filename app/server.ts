b76c40ec39dae28e1713873d81739e84c3f5b56f
Matheus Couto <couto.matheus@outlook.com>
2018-11-20 18:17:05 -0200

    build: using body-parser and cors

--- app/server.ts
+++ app/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: express.Application = express();
 
 const port: number = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
